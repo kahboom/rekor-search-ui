c705dbc584f2f7f559cdafd68ff1704dca8081a5
kahboom <rachelyordan@gmail.com>
2024-03-04 13:46:36 +0000

    fix(config): add bash script to set env var for rekor server
    fix: typo

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	modularizeImports: {
+		"@patternfly/react-icons": {
+			transform: "@patternfly/react-icons/{{member}}",
+		},
+	},
 	reactStrictMode: true,
-	transpilePackages: [
-		"@patternfly/react-core",
-		"@patternfly/react-styles"
-	],
+	transpilePackages: ["@patternfly/react-core", "@patternfly/react-styles"],
 };
 
 module.exports = nextConfig;
