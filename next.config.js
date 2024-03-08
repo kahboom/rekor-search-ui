98a6ae06062b5d3ec57ecdcb92d50c6282b143ad
kahboom <rachelyordan@gmail.com>
2024-03-08 22:00:27 +0000

    feat(config): add support for staging basePath + assetPrefix
    chore: set output to export
    
    ci: add env var for baseUrl when building to gh pages
    
    ci: echo NODE_ENV

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// assetPrefix: "/rekor-search-ui/",
-	// basePath: "/rekor-search-ui",
+	assetPrefix: process.env.NODE_ENV === "staging" ? "/rekor-search-ui/" : "",
+	basePath:
+		process.env.BASE_PATH || process.env.NODE_ENV === "staging"
+			? "/rekor-search-ui"
+			: "",
+	output: "export",
 	reactStrictMode: true,
 	transpilePackages: ["@patternfly/react-core", "@patternfly/react-styles"],
 };
